2b5535075c0500dc34cefdf2e5256ac742b0e310
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2023-05-03 13:18:42 +0530

    CLASS

---
+++ class.js
@@ -0,0 +1,9 @@
+class Student {
+    constructor(name, age, location) { 
+        this.name = name;
+        this.age = age;
+        this.location = location;
+     }
+}
+let student = new Student("Sumi", 26,"TNJ");
+console.log(student);
