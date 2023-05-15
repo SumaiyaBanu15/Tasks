b3d02a89a4fce6e6787de1626712820a9afb5978
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2023-05-15 19:43:23 +0530

    New

---
+++ FunctionsJS/function.js
@@ -0,0 +1,41 @@
+// let x = myFunction();
+// function myFunction(a,b){
+// return a*b;    
+// }
+// console.log(myFunction(2,5));
+// console.log(myFunction(3,5));
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit - 32);
+// }
+// let value = toCelsius(77);
+// console.log(value);
+// for passing muutiple values
+// console.log(toCelsius(77));
+// console.log(toCelsius(55));
+
+// function Hoisting
+// console.log(myFunction(5));
+// function myFunction(y){
+//     return (y*y);
+// }
+
+// self-invoking function
+// (function ()
+// {
+//     let x="hello";
+//     console.log(x);
+// })();
+
+// function can be used as value
+// function add(a,b){
+//     return a+b;
+// }
+// let x=add(2,8);
+// let x=add(2,8)*2;
+// console.log(x);
+let hello="";
+hello = val=> 
+  "hello!"+ val;
+
+console.log(hello("universe"));
